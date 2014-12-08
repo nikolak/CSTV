9d5735cbc7e1a568be2d48b20ced9050f56df56d
Nikola Kovacevic <nikolak@outlook.com>
2014-12-08 12:28:21 +0100

    Initial commit

---
+++ CSTV/Channel.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CSTV
+{
+    class Channel
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public string stream_params { get; set; }
+
+        public Channel(string extinf, string url)
+        {
+            this.name = parse_extinf(extinf);
+            this.url = url;
+            this.stream_params = get_stream();
+        }
+
+        private bool is_upper(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsLetter(input[i]) && !Char.IsUpper(input[i]))
+                    return false;
+            }
+            return true;
+        }
+
+        private string parse_extinf(string extinf)
+        {
+            string[] invalid_characters = { "?", "!", ".", ",", "-", "»", "«", "USA:", "()" };
+            //#EXTINF:-1 tvg-id="2164" tvg-name="BBC World News" tvg-logo="NEWS/BBC_World_News.png", BBC World News
+            string[] split_data = extinf.Split(',');
+            name = split_data[split_data.Length - 1];
+
+            foreach (string c in invalid_characters)
+            {
+                name = name.Replace(c, "");
+            }
+
+            int s = name.IndexOf('[');
+            int e = name.IndexOf(']');
+
+            while ((s != -1) && (e != -1))
+            {
+                name = name.Replace(name.Substring(s, e + 1 - s), "");
+                s = name.IndexOf('[');
+                e = name.IndexOf(']');
+            }
+
+            name = name.Trim();
+
+            if ((name.Length > 5) && (is_upper(name)))
+            {
+                return char.ToUpper(name[0]) + name.ToLower().Substring(1);
+            }
+
+            return name;
+        }
+
+        public string get_stream()
+        {
+            string source = this.url;
+
+            string[] split_data = source.Split(' ');
+
+            if (split_data.Length < 2)
+            {
+                return source;
+            }
+
+            source = source.Replace("playpath=", "--playpath ");
+            source = source.Replace("live=", "--live ");
+            source = source.Replace("pageUrl=", "--pageUrl ");
+            source = source.Replace("token=", "--token ");
+            source = source.Replace("timeout=", "--timeout ");
+            source = source.Replace("swfUrl=", "--swfUrl ");
+
+            split_data = source.Split(' ');
+
+            string result = source;
+            if (split_data[0].StartsWith("rtmp"))
+            {
+                result = String.Format("-r \"{0}\" ", split_data[0]);
+
+                string previous_value = "";
+
+                for (int i = 1; i < split_data.Length; i++)
+                {
+                    string value = split_data[i];
+                    if (value.StartsWith("--"))
+                    {
+                        result += value;
+
+                    }
+                    else
+                    {
+                        if (Regex.IsMatch(value, @"^\d+$") && previous_value == "--live")
+                        { //regex: true if string contains only digits
+                            result += value;
+                        }
+                        else
+                        {
+                            result += String.Format("\"{0}\"", value);
+                        }
+                    }
+
+                    result += " ";
+                    previous_value = value;
+
+                }
+
+            }
+            return result;
+        }
+    }
+
+}
---
+++ CSTV/Playlist.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSTV
+{
+    class Playlist
+    {
+        public List<Channel> channelList;
+
+        public Playlist(string location)
+        {
+            if (System.IO.File.Exists(location))
+            {
+                this.channelList = parsePlaylist(location);
+            }
+        }
+
+        private List<Channel> parsePlaylist(string playlistLocation)
+        {
+            List<Channel> playlistChannels = new List<Channel>();
+            string[] ignored_url_keywords = {"weebly", "devimages.apple.com", "thewiz.info", "radio", 
+                                                "0.0.0.0", "127.0.0.1"
+                                            };
+
+            string[] ignored_extinf_keywords = { "israel", "radio", "test", "brasil", 
+                                                   "spain", "argentina", "mexico", "uae", 
+                                                   "greece", "french", "bulgaria", "hungary", 
+                                                   "sweden", "italy", "italia", "(ita)"
+                                               };
+
+            List<string> playlist_data = new List<string>();
+
+            string file_line;
+            System.IO.StreamReader file = new System.IO.StreamReader(playlistLocation);
+            while ((file_line = file.ReadLine()) != null)
+            {
+                if (file_line.Trim() != "")
+                {
+                    playlist_data.Add(file_line);
+                }
+            }
+
+            file.Close();
+
+            for (int i = 0; i < playlist_data.Count - 1; i++)
+            {
+                if (playlist_data[i].StartsWith("#EXTINF") && !playlist_data[i + 1].StartsWith("#"))
+                {
+                    string extinf = playlist_data[i];
+                    string url = playlist_data[i + 1];
+                    bool valid = true;
+
+                    foreach (string word in ignored_extinf_keywords)
+                    {
+                        if (extinf.ToLower().Contains(word))
+                        {
+                            valid = false;
+                        }
+                    }
+
+                    foreach (string word in ignored_url_keywords)
+                    {
+                        if (url.ToLower().Contains(word))
+                        {
+                            valid = false;
+                        }
+                    }
+
+                    if (valid)
+                    {
+                        playlistChannels.Add(new Channel(extinf, url));
+                    }
+                }
+            }
+
+            return playlistChannels;
+        }
+
+    }
+}
